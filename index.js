ed0fc42cb99c4e719e69c133bb79020ec595547c
rob <rob@gmail.com>
2023-10-01 12:50:38 +0300

    little bit clean in index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// const AWS = require('aws-sdk');
-// AWS.config.update({
-//     region: 'eu-west-1',
-// });
-// const dynamodb = new AWS.DyncamoDB.DocumentClient();
-// const dynamodbTableName = 'product-inventory';
-// const healthPath = '/health';
-// const productPath = '/product';
-// const productsPath = '/products';
-
 exports.handler = async function(event) {
     console.log('Request event:', event);
     let response;
@@ -16,35 +6,6 @@ exports.handler = async function(event) {
             response = buildResponse(200);
             break;
 
-        // case event.httpMethod === 'GET' && event.path === productPath:
-        //     response = await getProduct(event.queryStringParameters.productId);
-        //     break;
-
-        // case event.httpMethod === 'GET' && event.path === productsPath:
-        //     response = await getProducts();
-        //     break;
-
-        // case event.httpMethod === 'POST' && event.path === productPath:
-        //     response = await saveProduct(JSON.parse(event.body));
-        //     break;
-
-        // case event.httpMethod === 'PATCH' && event.path === productPath:
-        //     const requestBody = JSON.parse(event.body)
-        //     response = await modifyProduct(
-        //         requestBody.productId,
-        //         requestBody.updateKey,
-        //         requestBody.updateValue,
-        //     );
-        //     break;
-        
-        // case event.httpMethod === 'DELETE' && event.path === productPath:
-        //     response = await deleteProduct(
-        //         JSON.parse(
-        //             event.body,
-        //         )
-        //         .productId,
-        //     );
-        //     break;
         default:
             response = buildResponse(404, '404, Not Found');
     }
@@ -74,98 +35,3 @@ async function scanDynamoRecords(scanParams, itemArray) {
         console.error('Your error handling here');
     }
 }
-
-// async function getProduct(id) {
-//     const params = {
-//         TableName: dynamodbTableName,
-//         Key: {
-//             'productId': id,
-//         }
-//     }
-//     return await dynamodb.get(params).promise().then(
-//         (response) => {
-//             return buildResponse(
-//                 200,
-//                 response.Item,
-//             )
-//         }, (error) => {
-//             console.log('Error handling here', error);
-//         }
-//     );
-// }
-
-// async function getProducts() {
-//     const params = {
-//         TableName: dynamodbTableName,
-//     }
-//     const allProducts = await scanDynamoRecords(params, []);
-//     const body = {
-//         products: allProducts,
-//     }
-//     return buildResponse(200, body);
-// }
-
-// async function saveProduct(requestBody) {
-//     const params = {
-//         TableName: dynamodbTableName,
-//         Item: requestBody
-//     }
-//     return await dynamodb.put(params).promise().then(
-//         () => {
-//             const body = {
-//                 Operation: 'SAVE',
-//                 Message: 'SUCCESS',
-//                 Item: requestBody,
-//             }        
-//             return buildResponse(200, body);
-//         }, (error) => {
-//             console.error('', error);
-//         }
-//     )
-// }
-
-// async function modifyProduct(id, key, value) {
-//     const params = {
-//         TableName: dynamodbTableName,
-//         Key: {
-//             'productId': id,
-//         },
-//         UpdateExpression: `set ${key} = :value`,
-//         ExpressionAttributeValues: {
-//             ':value': value,
-//         },
-//         ReturnValues: 'UPDATED_NEW',
-//     }
-//     return await dynamodb.update(params).promise().then(
-//         (response) => {
-//             const body = {
-//                 Operation: 'UPDATE',
-//                 Message: 'SUCCESS',
-//                 Item: response,
-//             }
-//             return buildResponse(200, body);
-//         }, (error) => {
-//             console.error('here error handling', error);
-//         }
-//     )
-// }
-
-// async function deleteProduct(id) {
-//     const params = {
-//         TableName: dynamodbTableName,
-//         Key: {
-//             'productId': id,
-//         },
-//         ReturnValues: 'ALL_OLD',
-//     }
-//     return await dynamodb.delete(params).promise().then(
-//         (response) => {
-//             const body = {
-//                 Operation: 'DELETE',
-//                 Message: 'SUCCESS',
-//                 Item: response,
-//             }
-//             return buildResponse(200, body);
-//         }
-//     )
-// }
